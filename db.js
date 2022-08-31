c455fc69c6d5031a0724d8e1747807ece53935d4
Aisha Soliman <aeshasoliman12@gmail.com>
2022-08-31 21:20:07 +0200

    add db and assets

---
+++ db.js
@@ -0,0 +1,122 @@
+[
+    {
+      "id": 1,
+      "image": "assets/python/1.png",
+      "title": " Learn Python: The Complete Python Programming Course",
+      "auther": "Avinash Jain, The Codex",
+      "starRate": 1.4,
+      "students": 1.247,
+      "price": 679.99,
+      "category": "python"
+    },
+    {
+      "id": 2,
+      "image": "assets/python/2.png",
+      "title": " Learn Python: The Complete Python Programming Course",
+      "auther": "Avinash Jain, The Codex",
+      "starRate": 4.4,
+      "students": 1.247,
+      "price": 679.99,
+      "category": "python"
+    },
+    {
+      "id": 3,
+      "image": "assets/python/3.png",
+      "title": " Learn Python: The Complete Python Programming Course",
+      "auther": "Avinash Jain, The Codex",
+      "starRate": 4.4,
+      "students": 1.247,
+      "price": 679.99,
+      "category": "python"
+    },
+    {
+      "id": 4,
+      "image": "assets/excel/1.png",
+      "title": " Learn Excel1: The Complete Excel Programming Course",
+      "auther": "Avinash Jain, The Codex",
+      "starRate": 4.4,
+      "students": 1.247,
+      "price": 679.99,
+      "category": "excel"
+    },
+    {
+      "id": 5,
+      "image": "assets/excel/2.png",
+      "title": " Learn Excel2: The Complete Excel Programming Course",
+      "auther": "Avinash Jain, The Codex",
+      "starRate": 4.4,
+      "students": 1.247,
+      "price": 679.99,
+      "category": "excel"
+    },
+    {
+      "id": 6,
+      "image": "assets/python/4.png",
+      "title": " Learn Python: The Complete Python Programming Course",
+      "auther": "Avinash Jain, The Codex",
+      "starRate": 2.4,
+      "students": 1.247,
+      "price": 679.99,
+      "category": "python"
+    },
+    {
+      "id": 7,
+      "image": "assets/excel/3.png",
+      "title": " Learn Excel4: The Complete Excel Programming Course",
+      "auther": "Avinash Jain, The Codex",
+      "starRate": 4.4,
+      "students": 1.247,
+      "price": 679.99,
+      "category": "excel"
+    },
+    {
+      "id": 8,
+      "image": "assets/excel/4.png",
+      "title": " Learn Excel2: The Complete Excel Programming Course",
+      "auther": "Avinash Jain, The Codex",
+      "starRate": 4.4,
+      "students": 1.247,
+      "price": 679.99,
+      "category": "excel"
+    },
+    {
+      "id": 9,
+      "image": "assets/web/1.png",
+      "title": " Learn Web Development: The Complete Web Development Programming Course",
+      "auther": "Avinash Jain, The Codex",
+      "starRate": 4.4,
+      "students": 1.247,
+      "price": 679.99,
+      "category": "web development"
+    },
+    {
+      "id": 10,
+      "image": "assets/web/2.png",
+      "title": " Learn Web Development: The Complete Web Development Programming Course",
+      "auther": "Avinash Jain, The Codex",
+      "starRate": 4.4,
+      "students": 1.247,
+      "price": 679.99,
+      "category": "web development"
+    },
+    {
+      "id": 11,
+      "image": "assets/web/3.png",
+      "title": " Learn Web Development: The Complete Web Development Programming Course",
+      "auther": "Avinash Jain, The Codex",
+      "starRate": 4.4,
+      "students": 1.247,
+      "price": 679.99,
+      "category": "web development"
+    },
+    {
+      "id": 12,
+      "image": "assets/web/4.png",
+      "title": " Learn Web Development: The Complete Web Development Programming Course",
+      "auther": "Avinash Jain, The Codex",
+      "starRate": 4.4,
+      "students": 1.247,
+      "price": 679.99,
+      "category": "web development"
+    }
+  ]
